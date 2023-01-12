73d5c700ef7f9cebc2f2f7b6b6aac858f06e6302
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-12 15:37:27 +0300

    Code fixed

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Практика C#
 string UserName = Console.ReadLine();
-Console.WriteLine('Приветствую вас на нашем портале');
-Console.Write('Введите ваше имя:');
+Console.WriteLine("Приветствую вас!");
+Console.Write("Введите ваше имя:");
 Console.ReadLine();
