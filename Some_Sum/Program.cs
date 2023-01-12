53bac6d980353969518278f9f2329a81c0bca5a1
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-12 16:18:05 +0300

    Random added

--- Some_Sum/Program.cs
+++ Some_Sum/Program.cs
@@ -2,3 +2,9 @@
 int numberB = 3;
 int Result = numberA + numberB;
 Console.WriteLine(Result);
+
+int NumberE = new Random().Next(1,10);
+Console.WriteLine(NumberE);
+int NumberF = new Random().Next(1,10);
+Console.WriteLine(NumberF);
+Console.WriteLine(NumberE + NumberF);
