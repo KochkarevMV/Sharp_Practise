cd2e9256f5cdc164a3f21ff71507348f8c55c043
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-12 16:30:21 +0300

    If_else added

---
+++ If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == Максим)
+{
+    Console.WriteLine("Welcome back, sir");
+}
+else
+{
+    Console.Write("Welcome, nice to see you, ");
+    Console.Write(username);
+}
