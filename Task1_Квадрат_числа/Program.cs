68151babf1b7d90b0d285ae9e83f64a422ae0197
KochkarevMV <ufomusic@mail.ru>
2023-01-13 11:32:58 +0300

    Задание 1 fixed 2

--- Task1_Квадрат_числа/Program.cs
+++ Task1_Квадрат_числа/Program.cs
@@ -2,6 +2,6 @@
 Console.Clear();
 Console.WriteLine("Введите число: ");
 Console.ReadLine();
-int a = int.Parse(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine();
 Console.Write("Квадрат числа: ");
 Console.Write($"{a}*{a}");
