15fc65a01203756caa4f915fb861814d0cc56e2e
KochkarevMV <ufomusic@mail.ru>
2023-01-17 10:45:47 +0300

    new random fixed 2

--- Task7_Последняя_цифра_числа/Program.cs
+++ Task7_Последняя_цифра_числа/Program.cs
@@ -10,8 +10,8 @@ Console.WriteLine(n % 10);
 Console.Clear();
 int n = new Random().Next(10, 100); // [10, 99]
 Console.WriteLine($"Случайное число: {n}");
-int n1 = n / 10;
-int n2 = n % 10;
+int n1 = n / 10; // находит первую цифру случайного числа
+int n2 = n % 10; // находит вторую цифру случайного числа
 if (n1 > n2)
     Console.WriteLine(n1);
 else
