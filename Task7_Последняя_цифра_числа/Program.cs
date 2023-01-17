4a7736981cd409aa54eb842386c748ade76956f5
KochkarevMV <ufomusic@mail.ru>
2023-01-17 10:40:05 +0300

    new random

--- Task7_Последняя_цифра_числа/Program.cs
+++ Task7_Последняя_цифра_числа/Program.cs
@@ -2,3 +2,16 @@
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(n % 10);
+
+
+//Случайное число из отрезка [10,99] и показывает большую его цифру
+
+Console.Clear();
+int n = new Random().Next(10, 100); // [10, 99]
+Console.WriteLine($"Случайное число: {n}");
+int n1 = n / 10;
+int n2 = n % 10;
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
