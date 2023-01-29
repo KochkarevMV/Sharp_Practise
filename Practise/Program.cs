a9864e51460ed11d992ee36434d734a8c89bb602
KochkarevMV <ufomusic@mail.ru>
2023-01-29 16:25:05 +0300

    Parctise 2

--- Practise/Program.cs
+++ Practise/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Рекурсия и двумерные массивы
+
+Console.Clear();
+string [,] words = new string [2,5];
+
+for (int i = 0; i < words.GetLength(0); i++)
+{
+    for (int j = 0; j < words.GetLength(1); j++)
+    Console.Write($"[{words[i,j]}]");
+Console.WriteLine();
+}
