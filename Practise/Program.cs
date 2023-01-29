58dc82a5e356d595c527908ce0a055c448e958ed
KochkarevMV <ufomusic@mail.ru>
2023-01-29 17:34:50 +0300

    Parctise 4

--- Practise/Program.cs
+++ Practise/Program.cs
@@ -1,26 +1,45 @@
 ﻿// Рекурсия и двумерные массивы
 
+
+
+// Заполнение и распечатка двумерного массива
+
+// Console.Clear();
+
+// void FillMatrix (int [,] fill)
+// {
+//     for (int i = 0; i < fill.GetLength(0); i++)
+// {
+//     for (int j = 0; j < fill.GetLength(1); j++)
+//     fill[i,j] = new Random().Next(5,20);
+// }
+// }
+
+// void PrintMatrix (int [,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     Console.Write($"[{matrix[i,j]}]");
+// Console.WriteLine();
+// }
+// }
+
+// int [,] matr = new int [2,5];
+// FillMatrix (matr);
+// PrintMatrix (matr);
+
+
+// Факториал через метод
+
 Console.Clear();
-
-void FillMatrix (int [,] fill)
+double Factorial (int n)
 {
-    for (int i = 0; i < fill.GetLength(0); i++)
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 20; i++)
+// int = 0 в начале приведёт к бесконечной рекурсии и ошибке Stack Overflow
 {
-    for (int j = 0; j < fill.GetLength(1); j++)
-    fill[i,j] = new Random().Next(5,20);
+    Console.WriteLine($" {i}! = {Factorial(i)}");
 }
-}
-
-void PrintMatrix (int [,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    Console.Write($"[{matrix[i,j]}]");
-Console.WriteLine();
-}
-}
-
-int [,] matr = new int [2,5];
-FillMatrix (matr);
-PrintMatrix (matr);
