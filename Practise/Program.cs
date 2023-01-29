853c44a94a5fa88ff09504e6a9e1fcedb8231d78
KochkarevMV <ufomusic@mail.ru>
2023-01-29 16:57:08 +0300

    Parctise 3

--- Practise/Program.cs
+++ Practise/Program.cs
@@ -1,11 +1,26 @@
 ﻿// Рекурсия и двумерные массивы
 
 Console.Clear();
-string [,] words = new string [2,5];
 
-for (int i = 0; i < words.GetLength(0); i++)
+void FillMatrix (int [,] fill)
 {
-    for (int j = 0; j < words.GetLength(1); j++)
-    Console.Write($"[{words[i,j]}]");
+    for (int i = 0; i < fill.GetLength(0); i++)
+{
+    for (int j = 0; j < fill.GetLength(1); j++)
+    fill[i,j] = new Random().Next(5,20);
+}
+}
+
+void PrintMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    Console.Write($"[{matrix[i,j]}]");
 Console.WriteLine();
 }
+}
+
+int [,] matr = new int [2,5];
+FillMatrix (matr);
+PrintMatrix (matr);
